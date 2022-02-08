f081f68f8f6094b3051488e394f5a9feb36ed80f
IFUNANYA ONAH <ifunanya.onah@thebulbafrica.institute>
2022-02-08 09:14:39 +0100

    Define the employee class with three private fields namely _StaffId, …

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace EmployeeManagement
+{
+
+
+    public class Program
+    {
+        public void Main()
+        {
+
+        }
+    }
+
+    public class Employee
+    {
+        private int _StaffId;
+        private string _FullName;
+        private DateTime _EmploymentDate;
+
+        public int staffId {    get { return _StaffId;} }
+
+        public string FullName { get { return _FullName;} }
+
+        public DateTime dateTime { get { return _EmploymentDate;} }
+
+        /// <summary>
+        /// A method that displays when an employee checks in at work. 
+        /// </summary>
+        public void ClockIn()
+        {
+            _EmploymentDate = DateTime.Now;
+            Console.WriteLine();
+        }
+
+
+    }
+}
